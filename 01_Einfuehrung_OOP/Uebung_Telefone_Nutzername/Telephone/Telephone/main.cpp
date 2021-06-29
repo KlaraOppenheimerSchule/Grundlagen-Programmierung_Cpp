01f6439b5b391742a6849aaec1cf9bb9d32f1381
Nutzername <sockeken@outlook.com>
2021-06-29 09:13:49 +0200

    initial commit

---
+++ 01_Einfuehrung_OOP/Uebung_Telefone_Nutzername/Telephone/Telephone/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Telephone
+//
+//  Created by Kris Myslowski on 29.06.21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
